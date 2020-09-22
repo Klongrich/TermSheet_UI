e3b5f84c000c4c3125ae3e8ee70cf7b2991b1735
Kyle Longrich <longrichk@gmail.com>
2020-09-22 00:21:46 -0400

    added basic functionality

--- src/App.js
+++ src/App.js
@@ -17,12 +17,36 @@ function App() {
   const [softCap, setSoftCap] = useState("500");
   const [supply, setSupply] = useState("300000");
 
-  const [staking, setStaking] = useState();
-  const [team, setTeam] = useState();
-  const [marketing, setMarketing] = useState();
-  const [total, setTotal] = useState();
+  const [staking, setStaking] = useState("30.00%");
+  const [team, setTeam] = useState("10.00%");
+  const [marketing, setMarketing] = useState("8.59%");
+  const [unlocked, setUnlocked] = useState("Marketing: 5%")
+  const [total, setTotal] = useState(36.00);
+
+  const [liqPercent, setLiqPercent] = useState("8")
 
   //const [presale, setPresale] = useState(); Always 30%
+
+  function calculate_outcome() {
+
+    get_total_distrubtion();
+    get_token_price();
+  }
+
+  function get_total_distrubtion() {
+
+    console.log(parseFloat(marketing));
+    console.log(parseFloat(staking));
+    console.log(parseFloat(team));
+
+    setTotal(((parseFloat(marketing) + parseFloat(staking) + parseFloat(team) + 36) - 100) * -1)
+  }
+
+  function get_token_price() {
+    return (
+      (parseFloat(supply) * (total / 100)) / (parseFloat(hardcap) * 0.5)
+    )
+  };
 
   return (
 
@@ -59,18 +83,24 @@ function App() {
 
         <li> Marketing <br />
           <input type="text" placeholder={marketing} onChange={e => setStaking(e.target.value)} />  
+        </li> 
+
+        <li> Unlocked <br />
+          <input type="text" placeholder={unlocked} onChange={e => setStaking(e.target.value)} />  
         </li> <br />
 
-        <li>Total: {total} </li> <br />
+        <li>Remaing: {total} </li> <br />
         
         <li>Presale: 30%</li>
         <li>liq (eth): </li>
         <li>liq (lid): </li>
-        <li>Fee 1%</li>
+        <li>LID Fee: 1%</li>
       </ul>
 
-    <h2> Bonus Range </h2> 
-      <ul>
+    <h2> Bonus Range </h2>
+    
+      <ul Style="float: left">
+        <li>Range</li>
         <li> 0 </li>
         <li> 250 </li>
         <li> 500 </li>
@@ -78,9 +108,43 @@ function App() {
         <li> 1250 </li>
       </ul>
 
-      <button>
+      <ul Style="float: left;">
+        <li> BasePrice </li>
+        <li> 77 </li>
+        <li> 74 </li>
+        <li> 71 </li>
+        <li> 68 </li>
+        <li> 65 </li>
+      </ul>
+
+      <ul Style="float: left;">
+        <li> Referal </li>
+        <li> 75 </li>
+        <li> 72 </li>
+        <li> 69 </li>
+        <li> 66 </li>
+        <li> 64 </li>
+      </ul>
+
+      <ul Style="display: inline-block">
+        <li> Tokens </li>
+        <li> 2300 </li>
+        <li> 2100 </li>
+        <li> 1890 </li>
+        <li> 1740 </li>
+        <li> 1570 </li>
+      </ul>
+
+      <div />
+
+      <button onClick={() => calculate_outcome()}
+              Style="display: inline-block">
         Submit
       </button>
+
+      <br /> 
+     <p> Token Price:  {get_token_price()} </p>
+     <p> Total Distribution: {total} </p>
 
     </>
     
