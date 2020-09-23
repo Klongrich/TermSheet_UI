487aec6b5977a79dcaf1d82ff6fd9bc2466b7673
Kyle Longrich <longrichk@gmail.com>
2020-09-22 22:08:21 -0400

    updated styles, will refactor boxes into seprate components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React , {useEffect, useState} from 'react';
 import styled from "styled-components";
+import { CSVLink } from "react-csv";
 
-import { CSVLink, CSVDownload } from "react-csv";
-
+import LidLogo from './images/logo-lid.png';
 
 const Container = styled.div`
-  font-family: Gotham, sans-serif;
+  font-family: 'Gotham', sans-serif;
+  background-color: #E4E4E4;;
+  margin-top: -20px;
+  padding-top: 20px;
+
+  div {
+    background-color: #4A4A4A;
+    padding-right: 50px;
+    border: 2px solid #E4E4E4;
+    border-radius: 15px;
+    color: #E4E4E4;
+    margin-left: 20px;
+    margin-right: 20px;
+  }
   
   ul {
     list-style-type: none;
@@ -30,12 +43,15 @@ const Container = styled.div`
 
 const ResultsContainer = styled.div`
   font-family: Gotham, sans-serif;
+  background-color:#737373;
   
-  margin-top: 20px;
+  margin-top: 5px;
+  margin-bottom: 40px;
 
   ul {
-    margin-right: 200px;
-    margin-top: 0px;
+    margin-right: 80px;
+    margin-top: -5px;
+    width: 250px;
   }
 
   h2 {
@@ -64,7 +80,7 @@ function App() {
   const [staking, setStaking] = useState("30.00%");
   const [team, setTeam] = useState("10.00%");
   const [marketing, setMarketing] = useState("8.59%");
-  const [unlocked, setUnlocked] = useState("Marketing: 5%")
+  const [unlocked, setUnlocked] = useState("Marketing")
 
   const [ethLiq, setEthliq] = useState(12.83);
   const [lidLiq, setLidliq] = useState(2.57)
@@ -119,7 +135,7 @@ function App() {
     ["liq (xxx/LID)", lidLiq],
     ["Team", team ],
     ["Marketing", marketing],
-    ["Unlocked: 5%", "Marketing "],
+    ["Unlocked: 5%", unlocked],
     ["", ""],
     ["Ether Distribution (%)"],
     ["Fund", "Quantity", "Remaining", softCap, hardcap],
@@ -134,13 +150,23 @@ function App() {
 
     <>
     <Container>
-      <h2>Term Sheet App </h2>
+    <img Style="width: 50px;
+                height: 50px;
+                margin-left: 20px;
+                margin-bottom: -70px;"src={LidLogo} />
+
+      <h2 Style=" font-size: 28px;
+                  color: #0c65EB;
+                  margin-left: 90px;
+                  ">
+                  Term Sheet App </h2>
 
       <div Style="float: left;
-                  margin-right: 70px;"> 
-      <h2>Inital</h2>
+                  background: linear-gradient(0deg, rgba(12,101,235,1) 0%, rgba(28,158,247,1) 100%);
+                  color: white;"> 
+      <h2>Token Presale</h2>
       <ul>
-        <li> HardCap: <br />
+        <li> HardCap <br />
           <input type="number" 
                   placeholder={hardcap} 
                   step="0.01"
@@ -148,7 +174,7 @@ function App() {
         </li>
 
 
-        <li>SoftCap: <br />
+        <li>SoftCap <br />
           <input type="number" 
                  placeholder={softCap} 
                  step="0.01"
@@ -156,14 +182,14 @@ function App() {
         </li>
 
 
-        <li>Supply: <br />
+        <li>Supply <br />
           <input  type="number" 
                   placeholder={supply} 
                   step="0.01"
                   onChange={e => setSupply(e.target.value)} />
         </li>
 
-        <li>LIQ: <br />
+        <li>LIQ <br />
           <input  type="number" 
                   placeholder={liq}
                   step="0.01" 
@@ -175,10 +201,11 @@ function App() {
       </div>
 
   <div Style="float: left;
-              margin-right: 50px;">
+              background: linear-gradient(0deg, rgba(12,101,235,1) 0%, rgba(28,158,247,1) 100%);
+              color: white;">
       <h2> Token Distribution </h2>
       <ul>
-        <li>Staking: <br />
+        <li>Staking <br />
           <input  type="number" 
                   placeholder={staking}
                   step="0.01" 
@@ -203,15 +230,19 @@ function App() {
           <input type="text" 
                  placeholder={unlocked}
                  onChange={e => setUnlocked(e.target.value)} />  
-        </li> <br />
-
-        <li>Remaing: {(get_total_distrubtion() - 100) * -1}% </li> <br />
+        </li> 
+        
+        {/*
+        <br />
+        <li>Remaing: {(get_total_distrubtion() - 100) * -1}% </li> <br /> */}
 
       </ul>
     </div>
 
   <div Style="display:inline-block;
-            margin-bottom: 100px;">
+            margin-bottom: 100px;
+            background-color: white;
+            color: #0c65EB">
     <h2> Bonus Range </h2>
     
       <ul Style="float: left">
@@ -259,7 +290,7 @@ function App() {
         <li>Staking: {staking}</li>
         <li>Team: {team}</li>
         <li>Marketing: {marketing} </li>
-        <li>Unlocked: marketing 5% </li>
+        <li>Unlocked: {unlocked} 5% </li>
     </ul>
 
     <ul Style="float: left">
@@ -280,15 +311,25 @@ function App() {
 
       <br /> 
 
-      <div Style="margin-left: 40px;
-                  padding-top: 10px;"> 
-        <CSVLink data={csvData}>Download me</CSVLink>
-      </div>
-
-       <button onClick={() => calculate_outcome()}
-              Style="display: inline-block">
+       <button Style="font-size: 18px;
+                      display: inline-block;
+                      border:2px solid #D8E0E7;
+                      border-radius:25px;
+                      background-color: #D8E0E7;
+                      width: 173px;
+                      height: 35px;
+                      margin-bottom:20px;
+                      margin-left: 30px;
+                      z-index: 2;
+                      "
+       onClick={() => calculate_outcome()}
+      >
         Submit
       </button>  
+
+    <br />
+
+      {/* <CSVLink data={csvData}>Download CSV</CSVLink>    */}
       
       </ResultsContainer> 
     </Container>
