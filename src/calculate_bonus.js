092888b8491fd7e44d7a105db6ea3d5b501e62dc
Kyle Longrich <longrichk@gmail.com>
2020-09-23 23:03:32 -0400

    starting bonus calculation function

---
+++ src/calculate_bonus.js
@@ -0,0 +1,9 @@
+import React , {useEffect, useState} from 'react';
+
+const bounusRange = [25, 20, 15, 10];
+const bounsAmounts = [200, 300, 500, 250];
+
+export default function get_liq_percentage(token_supply, ETH_HardCap){
+
+
+}
