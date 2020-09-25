0301c8d22bf4414de8406db6f669a503fc54fcb4
Kyle Longrich <longrichk@gmail.com>
2020-09-24 23:40:42 -0400

    Updates

---
+++ precache-manifest.50c1c70de487e4fcd72ac1918f8bd1fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fa8ad6084def687063770089bdd0c99",
+    "url": "/TermSheet_UI/index.html"
+  },
+  {
+    "revision": "a4cab70b89fba109043f",
+    "url": "/TermSheet_UI/static/css/main.f4cc4fb6.chunk.css"
+  },
+  {
+    "revision": "fd7c89c0fe2a77ebe845",
+    "url": "/TermSheet_UI/static/js/2.3ac7e164.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TermSheet_UI/static/js/2.3ac7e164.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4cab70b89fba109043f",
+    "url": "/TermSheet_UI/static/js/main.1dda07c2.chunk.js"
+  },
+  {
+    "revision": "bf542520819689bcb2b3",
+    "url": "/TermSheet_UI/static/js/runtime-main.dc098adc.js"
+  },
+  {
+    "revision": "20b6644e35cb71db59f9a847397f9d1e",
+    "url": "/TermSheet_UI/static/media/logo-lid.20b6644e.png"
+  }
+]);
