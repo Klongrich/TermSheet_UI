518037241a8fe6766bea6c62a7b592fc93f1aa05
Kyle Longrich <longrichk@gmail.com>
2020-10-01 16:05:28 -0400

    added Timer paramters

--- server/create_json.js
+++ server/create_json.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -10,20 +11,35 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/Timer', function(req, res, next) {
+  
+  let data = { 
+    startTime: req.startTime, 
+    hardCapTimer: req.hardCapTimer, 
+    softcape: "ether(" + req.softCap + ")"
+  }
+    
+  fs.writeFile("Timer.json", JSON.stringify(data), err => {  
+    if (err) throw err;  
+    console.log("Done writing"); 
+  }) 
   console.log(req);
 })
 
-app.get('/Redemr', function(req, res, next){
+app.get('/Redemer', function(req, res, next){
+
   console.log(req);
 })
 
 app.get('/Presale', function(req, res, next) {
+
   console.log(req);
 })
 
 app.get('/TimeLock', function(req, res, next) {
+  
   console.log(req);
 })
+
 
 app.listen(3010, () => {
     console.log("Started Server On Port 3010");
